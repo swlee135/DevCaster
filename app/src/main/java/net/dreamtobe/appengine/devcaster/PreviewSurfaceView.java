a31972d3a887e102d4ab1e285d15dbe450ae5c96
이승원 <andrew.lee@dreamtobe.net>
2015-11-19 11:49:10 +0900

    VACaster SDK를 위한 클래스 추가

--- app/src/main/java/net/dreamtobe/appengine/devcaster/PreviewSurfaceView.java
+++ app/src/main/java/net/dreamtobe/appengine/devcaster/PreviewSurfaceView.java
@@ -1,13 +1,38 @@
 package net.dreamtobe.appengine.devcaster;
 
 import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by swlee on 2015-11-13.
  */
-public class PreviewSurfaceView extends SurfaceView {
+public class PreviewSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    Camera      mCamera;
+
     public PreviewSurfaceView(Context context) {
         super(context);
+        mCamera = null;
+        getHolder().addCallback(this);
+    }
+
+    public Camera getCamera() {
+        return mCamera;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
--- app/src/main/java/net/dreamtobe/appengine/devcaster/VACaster.java
+++ app/src/main/java/net/dreamtobe/appengine/devcaster/VACaster.java
@@ -1,32 +1,41 @@
 package net.dreamtobe.appengine.devcaster;
 
 /**
- * Created by swlee on 2015-11-13.
+ * Video Upstreaming을 위한 Class
+ * @author 이승원 <andrew.lee@dreamtobe.net>
+ * @version 0.0.1
+ * @since  2015-11-19
  */
 public class VACaster {
     /// Preview를 위한 SurfaceView 객체
     private PreviewSurfaceView mSurfaceView;
 
-    public VACaster() {
-    }
-
+    /**
+     * VACaster 생성자
+     * <p>
+     * Video Upstreaming을 위한 VACaster 기본 생성자
+     * <p>
+     * @param surfaceView Preview를 랜더링할 SurfaceView로 PreviewSurfaceView객체를 사용
+     */
     public VACaster(PreviewSurfaceView surfaceView) {
         mSurfaceView = surfaceView;
     }
 
-    public int StartPreview() {
+    /**
+     * Preview 영상 UpStreaming을 시작
+     * <p>
+     * 카메라 Preview 영상을 RTSP/RTP를 이용하여 지정된 서버에 Upstreaming 한다.
+     * @param url VAAllocator를 통해 지정된 VAController IP, Port 정보 IP:Port 형태로 전달된다.
+     * @param commands VA 명령 조합, 장소,얼굴,표정,객체 인식 명령 조합으로 구성
+     * @param authToken
+     * @param parameter
+     * @return
+     */
+    public int startUpStreaming(String url, int commands, String authToken, String parameter) {
         return 0;
     }
 
-    public int StopPreview() {
-        return 0;
-    }
-
-    public int StartUpStreaming() {
-        return 0;
-    }
-
-    public int StopUpStreaming() {
+    public int stopUpStreaming() {
         return 0;
     }
 
